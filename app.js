839315b7071ef96b998b7792cb4aa47c54f63220
Ege Ayan <ayanege2001@gmail.com>
2024-10-08 19:47:41 +0300

    send test data to db

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
+const Campground = require("./models/campground");
 
 mongoose.connect("mongodb://localhost:27017/camper");
 
@@ -17,6 +18,12 @@ app.set("views", path.join(__dirname, "views"));
 
 app.get("/", (request, response) => {
   response.render("home");
+});
+
+app.get("/makecampground", async (request, response) => {
+  const camp = new Campground({ title: "Test ", description: "Test Message" });
+  await camp.save();
+  response.send(camp);
 });
 
 app.listen(3000, () => {
