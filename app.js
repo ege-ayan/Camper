8faf8db9a487de60b0de5ebea2a4112b9ad95fef
Ege Ayan <ayanege2001@gmail.com>
2024-10-08 19:41:21 +0300

    mongoose initialization

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const express = require("express");
-const app = express();
 const path = require("path");
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/camper");
+
+const database = mongoose.connection;
+database.on("error", console.error.bind(console, "connection error:"));
+database.once("open", () => {
+  console.log("Database Connected");
+});
+
+const app = express();
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
