5acb74f24e37aff05a455c25fc29b070787baa56
Ege Ayan <ayanege2001@gmail.com>
2024-10-08 20:53:16 +0300

    seeding logic added

--- seeds/index.js
+++ seeds/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
 const Campground = require("../models/campground");
+const cities = require("./cities");
+const { places, descriptors } = require("./seedHelper");
 
 mongoose.connect("mongodb://localhost:27017/camper");
 
@@ -12,10 +14,20 @@ database.once("open", () => {
   console.log("Database Connected");
 });
 
-const seedDB = async () => {
-  await Campground.deleteMany({});
-  const c = new Campground({ title: "purple field" });
-  await c.save();
+const sample = (array) => {
+  return array[Math.floor(Math.random() * array.length)];
 };
 
-seedDB();
+const seedDB = async () => {
+  await Campground.deleteMany({});
+  for (let i = 0; i < 50; i++) {
+    const random1000 = Math.floor(Math.random() * 1000);
+    const camp = new Campground({
+      location: `${cities[random1000].city}, ${cities[random1000].state}`,
+      title: `${sample(descriptors)} ${sample(places)}`,
+    });
+    await camp.save();
+  }
+};
+
+seedDB().then(() => mongoose.connection.close());
